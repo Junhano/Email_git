50cc8accb78fa2433bd0df2b88065b1b816617cf
Junhan Ouyang <junhano@uci.edu>
2020-04-07 23:28:46 -0700

    Give user ability to set word limit

--- main.py
+++ main.py
@@ -7,17 +7,24 @@ import tkinter as tk
 import smtplib
 
 
+
 def find_word_length(text):
     return len([i for i in text.split() if i != '\n'])
+
+
 
 class Email_app(tk.Frame):
     def __init__(self, master = None):
         tk.Frame.__init__(self, master)
+        self.word_limit = 200
         self.create_widget()
+        
 
     def create_widget(self):
         
-        self.myLabel = tk.Label(root, text = "Email app", width = 50).grid()
+        self.myLabel = tk.Label(root, text = "Email app").place(x = 170, y = 5, width = 60, height = 20)
+        self.setting = tk.Button(root, text = "setting", command = self.pop_up_word_limit_setting)
+        self.setting.place(x = 300, y = 5, width = 70, height = 20)
         self.myUsername = tk.Label(root, text = "Username").place(x = 10, y = 30, width = 70, height = 20)
         self.myUserEntry = tk.Entry(root)
         self.myUserEntry.place(x = 90, y = 30, width = 200, height = 20)
@@ -37,8 +44,16 @@ class Email_app(tk.Frame):
         
         self.strVal = tk.StringVar() 
         self.strVal.set("Word: 0 word")
-        self.Textlength = tk.Label(root, textvariable = self.strVal, width = 70)
-        self.Textlength.place(x = 320, y = 50, width = 120, height = 30)
+        self.Textlength = tk.Label(root, textvariable = self.strVal)
+        self.Textlength.place(x = 320, y = 70, width = 120, height = 30)
+        
+        
+        self.wordLimitDisplay = tk.StringVar()
+        self.wordLimitDisplay.set(f"Current word limit {self.word_limit} words")
+        self.LimitDisplay = tk.Label(root, textvariable = self.wordLimitDisplay)
+        self.LimitDisplay.place(x = 290, y = 50, width = 160, height = 30)
+        
+        
 
         self.myBtn = tk.Button(root,text = "Submit email", command = self.on_release)
         self.myBtn.place(x = 350, y = 600)
@@ -63,12 +78,27 @@ class Email_app(tk.Frame):
         length = find_word_length(self.myEntry.get('1.0',tk.END))
         if length == 1 or length == 0:
             self.strVal.set(f"Word: {length} word")
-        elif length > 1 and length <= 200:
+        elif length > 1 and length <= self.word_limit:
             self.strVal.set(f"Word: {length} words")
-        if length > 200:
+        if length > self.word_limit:
             self.myEntry.delete("end-2c")
     
 
+    def pop_up_word_limit_setting(self):
+        popup = tk.Toplevel()
+        popup.title("Word Limit Setting")
+        tk.Label(popup, text = "Enter the word limit you want below").grid(row = 0, column = 0)
+        word_limit = tk.Entry(popup)
+        word_limit.grid(row = 1, column = 1)
+        tk.Button(popup, text = 'save', command = lambda : action(popup)).grid(row = 2, column = 3)
+        
+        def action(root):
+            try:
+                self.word_limit = int(word_limit.get())
+                self.wordLimitDisplay.set(f"Current word limit {self.word_limit} words")
+                root.destroy()
+            except:
+                pass
 
     
 root = tk.Tk()
